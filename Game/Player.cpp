a55f520dd14f4365476e0dc080450e842e270e3f
takuya yamamoto <takuyahappiness404@gmail.com>
2019-07-11 15:25:04 +0900

    チンチン

--- Game/Player.cpp
+++ Game/Player.cpp
@@ -76,7 +76,9 @@ void Player::Update()
 	if (Pad(0).IsPress(enButtonA)&& m_timer >= 10) {
 		Bullet* bullet = NewGO<Bullet>(0, "Pbullet");
 		bullet->m_position = m_position;
-		bullet->m_movespeed.z = 30.0f;
+		bullet->m_movespeed.z = MainCamera().GetForward().z*30.0f;
+		bullet->m_movespeed.x = MainCamera().GetForward().x*30.0f;
+		bullet->m_skinrender->SetRotation(m_rotation);
 		m_sound = NewGO<prefab::CSoundSource>(0);
 		m_sound->Init(L"sound/shoot.wav");
 		m_sound->Play(false);
